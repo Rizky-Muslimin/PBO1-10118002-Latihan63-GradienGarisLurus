c58efa754cc7e2c635a83a4072b7f52ceae8b922
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-10-24 20:19:01 +0700

    Add files via upload

---
+++ PBO1-10118002-Latihan63-GradienGarisLurus/src/pbo1/pkg10118002/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,13 @@
+
+package pbo1.pkg10118002.latihan63.gradiengarislurus;
+
+/**
+ * NAMA     : Rizky Muslimin
+ * KELAS    : IF-1
+ * NIM      : 10118002
+ */
+public interface GarisLurus {
+    
+    public int hitungGradien();
+    
+}
